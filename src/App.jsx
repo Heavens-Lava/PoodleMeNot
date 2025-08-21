cbbe3db0072a34195656d6331eec00f860712b1b
Jeffrey Macy <yosoytujeffe@gmail.com>
2025-08-21 15:11:54 -0700

    fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import { AnimatePresence } from "framer-motion";
 
 // Import pages
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
